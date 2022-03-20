804e6ba96564d3bc6f24515395a34e542fa17dad
Zaid Rashid <zed182@gmail.com>
2022-03-20 18:46:35 +0100

    Update user controller test spec

--- src/users/users.controller.spec.ts
+++ src/users/users.controller.spec.ts
@@ -1,12 +1,35 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { AuthService } from './auth.service';
+import { UsersService } from './users.service';
+import { User } from './user.entity';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeAuthService = {
+      signup: () => Promise.resolve({ id: 1 } as User),
+      signin: () => Promise.resolve({ id: 1 } as User),
+    };
+    fakeUsersService = {
+      findOne: () => Promise.resolve({} as User),
+      find: () => Promise.resolve([{} as User]),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: fakeAuthService,
+        },
+        {
+          provide: UsersService,
+          useValue: fakeUsersService,
+        },
+      ],
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
