29d13b84e0c0953e128d2f81502a1014ad099247
Zaid Rashid <zed182@gmail.com>
2022-03-21 21:27:50 +0100

    Create e2e auth test

---
+++ test/auth.e2e-spec.ts
@@ -0,0 +1,30 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { INestApplication } from '@nestjs/common';
+import * as request from 'supertest';
+import { AppModule } from './../src/app.module';
+
+describe('Authentication System (e2e)', () => {
+  let app: INestApplication;
+
+  beforeEach(async () => {
+    const moduleFixture: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    }).compile();
+
+    app = moduleFixture.createNestApplication();
+    await app.init();
+  });
+
+  it('signup request', () => {
+    const testEmail = 'test@email.com';
+    return request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email: testEmail, password: '123' })
+      .expect(201)
+      .then((res) => {
+        const { id, email } = res.body;
+        expect(id).toBeDefined();
+        expect(email).toBe(testEmail);
+      });
+  });
+});
