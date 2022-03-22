414c1d4388f9499cd5d7e16c830ca242f8022ab3
Zaid Rashid <zed182@gmail.com>
2022-03-22 22:38:08 +0100

    Add create reports functionality

--- src/reports/reports.controller.spec.ts
+++ src/reports/reports.controller.spec.ts
@@ -1,12 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Report } from './report.entity';
 import { ReportsController } from './reports.controller';
+import { ReportsService } from './reports.service';
 
 describe('ReportsController', () => {
   let controller: ReportsController;
+  let fakeReportsService: Partial<ReportsService>;
 
   beforeEach(async () => {
+    fakeReportsService = {
+      create: () => Promise.resolve({} as Report),
+    };
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ReportsController],
+      providers: [
+        {
+          provide: ReportsService,
+          useValue: fakeReportsService,
+        },
+      ],
     }).compile();
 
     controller = module.get<ReportsController>(ReportsController);
@@ -14,5 +26,19 @@ describe('ReportsController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('fail if body is not correct', () => {
+    try {
+      controller.createReport({
+        make: 'test make',
+        model: 'test model',
+        price: 0,
+        year: 0,
+        mileage: 0,
+        lng: 0,
+        lat: 0,
+      });
+    } catch (err) {}
   });
 });
