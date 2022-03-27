2a2040f6b17f1d9592f7a3846675380474ba4953
Zaid Rashid <zed182@gmail.com>
2022-03-27 13:04:48 +0200

    Insert test reports sql

---
+++ queries/insert_multiple_report.sql
@@ -0,0 +1,8 @@
+-- SQLite
+INSERT INTO report (id, price, approved, make, model, year, mileage, lng, lat, userId)
+VALUES 
+(6,20000,0,'toyota','supra',1950,40234,101.6899,3.139,1),
+(7,5000,0,'toyota','supra',1956,43000,103.6899,1.3521,1),
+(8,3000,0,'toyota','supra',1956,70000,6.9603,50.9375,1),
+(9,5100,0,'toyota','supra',1952,30000,115.8613,-31.9523,1),
+(10,5100,0,'toyota','supra',1952,30000,101.8613,3.9523,1);
